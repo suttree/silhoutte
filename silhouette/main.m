bd2780a8cce09f1a95eba214dd91955252127f16
Duncan Gough <duncan@suttree.com>
2011-06-02 21:34:34 +0100

    Initial import

---
+++ silhouette/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  silhouette
+//
+//  Created by Duncan Gough on 02/06/2011.
+//  Copyright SwitchPlayed 2011. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppDelegate");
+    [pool release];
+    return retVal;
+}
